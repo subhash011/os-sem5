5bf0c5ee18b93b4a99fba2ef65c6fb9bd7d8289e
subhash011 <subhash011011@gmail.com>
2020-10-19 12:57:44 +0530

    changed test cases for more readability

--- xv6/test_v2paddr.c
+++ xv6/test_v2paddr.c
@@ -5,7 +5,7 @@
 void printmsg(int status, uint v2p, void* vaddr) {
     switch (status) {
         case -1:
-            printf(1, "Page not found or address 0x%x not accessible from user space.\n", vaddr);
+            printf(1, "Page not found or not enough permissions to access address 0x%x.\n", vaddr);
             break;
         default:
             printf(1, "Virtual address: 0x%x -> Physical address: 0x%x\n", vaddr, v2p);
@@ -18,29 +18,43 @@ int
 main(int argc, char *argv[])
 {
     int num = 0;
-    uint v2p_num;
-    uint v2p_globalnum;
-    uint v2p_invalid;
-    uint v2p_kernaddr;
-    uint v2p_devmem;
-    uint v2p_temp;
-    int s_num = v2paddr(&v2p_num, &num);
-    int s_globalnum = v2paddr(&v2p_globalnum, &globalnum);
-    void* invalid = 0x0;
-    int s_invalid = v2paddr(&v2p_invalid, invalid);
-    int a[] = {1, 2, 3, 4};
-    void* kernaddr = (void *) (KERNBASE + 0x1);
-    int s_kernaddr = v2paddr(&v2p_kernaddr, kernaddr);
-    void* devmem = (void *) (DEVSPACE + 0x1);
-    int s_devmem = v2paddr(&v2p_devmem, devmem);
+    // virtual addresses
+    void *random, *kernaddr, *devmem;
+    // physical addresses
+    uint v2p_num, v2p_globalnum, v2p_random,
+         v2p_kernaddr, v2p_devmem, v2p_temp;
+    // status codes
+    int s_num, s_globalnum, s_random, s_kernaddr,
+        s_devmem, s_temp;
+    random  = (void *) 0x345;
+    kernaddr = (void *) (KERNBASE + 0x1);
+    devmem = (void *) (DEVSPACE + 0x1);
+
+    // local variable va to pa.
+    s_num = v2paddr(&v2p_num, &num);
     printmsg(s_num, v2p_num, &num);
+
+    // global variable va to pa.
+    s_globalnum = v2paddr(&v2p_globalnum, &globalnum);
     printmsg(s_globalnum, v2p_globalnum, &globalnum);
-    printmsg(s_invalid, v2p_invalid, invalid);
+
+    // just a random integer va to pa.
+    s_random = v2paddr(&v2p_random, random);
+    printmsg(s_random, v2p_random, random);
+
+    // kernel space va to pa.
+    s_kernaddr = v2paddr(&v2p_kernaddr, kernaddr);
     printmsg(s_kernaddr, v2p_kernaddr, kernaddr);
+
+    // device memory space va to pa.
+    s_devmem = v2paddr(&v2p_devmem, devmem);
     printmsg(s_devmem, v2p_devmem, devmem);
+
+    // array va to pa.
     printf(1, "===== Array elements start =====\n");
+    int a[] = {1, 2, 3, 4};
     for (int i = 0; i < sizeof(a)/sizeof(a[0]); i++) {
-        int s_temp = v2paddr(&v2p_temp, &a[i]);
+        s_temp = v2paddr(&v2p_temp, &a[i]);
         printmsg(s_temp, v2p_temp, &a[i]);
     }
     exit();
